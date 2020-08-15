273d02e4cfddb1730b3f5148f0a2ad579bfb17b6
Ilian Ranguelov <me@radarlog.net>
2020-08-16 00:06:04 +0200

    update dependencies

--- config/routes.php
+++ config/routes.php
@@ -8,7 +8,8 @@ use Slim\Routing\RouteCollectorProxy;
 
 return static function (App $app): void {
     // phpcs:disable SlevomatCodingStandard.Functions.StaticClosure
-    $app->group('/api/v1', function (RouteCollectorProxy $group): void {
-        $group->get('/time/{timestamp:[0-9]+}', Controller\Timestamp::class);
-    });
+    $app->group(
+        '/api/v1',
+        fn(RouteCollectorProxy $group) => $group->get('/time/{timestamp:[0-9]+}', Controller\Timestamp::class),
+    );
 };
